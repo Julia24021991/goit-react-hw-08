9701d1ba4ab4df2aecd8b983b85ba05a28f11550
Julia24021991 <yuliya.kovalyk@gmail.com>
2024-03-13 20:12:51 +0200

    commit12

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,21 @@
-import { useId } from "react";
-import css from "./SearchBox.module.css"
+import { useId } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterNameNumber } from '../../redux/contacts/filterSlice';
+import { selectFilter } from '../../redux/contacts/selectors';
+import css from './SearchBox.module.css';
 
-export const SearchBox = ({ value, onChange }) => {
-    const id = useId();
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const handleChange = event => dispatch(filterNameNumber(event.target.value));
+  const value = useSelector(selectFilter);
+  const id = useId();
 
-    return (
-        <div className={css.searchBar}>
-            <label htmlFor={id}>Find contact by name</label>
-            <input type="text"
-                id={id}
-                value={value}
-                onChange={event => onChange(event.target.value)} />
-        </div>
-    )
+  return (
+    <div className={css.filter}>
+      <label htmlFor={id}>Find contact</label>
+      <input type="text" id={id} value={value} onChange={handleChange} className={css.input} />
+    </div>
+  );
+};
 
-}
+export default SearchBox;
