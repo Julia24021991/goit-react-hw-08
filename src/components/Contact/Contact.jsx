1c0f8688d280ebe7b33a235e87e06e8af21cc8b1
Julia24021991 <yuliya.kovalyk@gmail.com>
2024-03-13 19:56:46 +0200

    commit10

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,37 @@
-import { FaUser, FaPhone } from "react-icons/fa6";
-import css from "./Contact.module.css"
+import { FaUser, FaPhone } from 'react-icons/fa6';
+import css from './Contact.module.css';
 
-export const Contact = ({ contact: { name, number, id }, onDeleteButton }) => {
-    return (
-        <div className={css.contact}>
-            <div>
-                <p className={css.name}><FaUser className={css.icon} />{name}</p>
-                <p className={css.number}><FaPhone className={css.icon} />{number}</p>
-            </div>
-            <button className={css.button} onClick={() => onDeleteButton(id)}>Delete</button>
-        </div>
-    )
-}
+export const Contact = ({ contact: { name, number }, openDeleteModal, openPatchModal }) => {
+  return (
+    <div className={css.item}>
+      <div>
+        <p className={css.name}>
+          <FaUser className={css.icon} />
+          {name}
+        </p>
+        <p className={css.number}>
+          <FaPhone className={css.icon} />
+          {number}
+        </p>
+      </div>
+      <div className={css.buttons}>
+        <button
+          className={css.button}
+          onClick={() => {
+            openDeleteModal();
+          }}
+        >
+          Delete
+        </button>
+        <button
+          className={css.button}
+          onClick={() => {
+            openPatchModal();
+          }}
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,87 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useId } from "react";
-import * as Yup from "yup";
-import css from "./ContactForm.module.css"
-import { nanoid } from 'nanoid'
+import { useId } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import toast from 'react-hot-toast';
+import css from './ContactForm.module.css';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/contacts/operations';
 
 const contactSchema = Yup.object().shape({
-    name: Yup.string()
-        .min(3, "Too short! Min 3 symbols")
-        .max(50, "Too long! Max 50 symbols")
-        .required("Required"),
-    number: Yup.string()
-        .min(3, "Too short! Min 3 symbols")
-        .max(50, "Too long! Max 50 symbols")
-        .required("Required"),
-})
+  name: Yup.string()
+    .min(3, 'Too short! Min 3 symbols')
+    .max(50, 'Too long! Max 50 symbols')
+    .required('Required'),
+  number: Yup.string()
+    .min(3, 'Too short! Min 3 symbols')
+    .max(50, 'Too long! Max 50 symbols')
+    .required('Required'),
+});
 
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+  const nameFieldId = useId();
+  const numberFieldId = useId();
 
-export const ContactForm = ({ onAdd }) => {
-    const nameFieldId = useId();
-    const numberFieldId = useId();
+  const handleSubmit = (values, actions) => {
+    dispatch(addContact({ ...values }))
+      .unwrap()
+      .then(() => {
+        toast.success('Contact successfully added!', {
+          style: {
+            border: '1px solid #0d47a1',
+            padding: '16px',
+            color: '#111',
+          },
+          iconTheme: {
+            primary: '#2196f3',
+            secondary: '#fff',
+          },
+        });
+        actions.resetForm();
+      })
+      .catch(() => {
+        toast.error('Oops, something go wrong!', {
+          style: {
+            border: '1px solid #F1041B',
+            padding: '16px',
+            color: '#111',
+          },
+          iconTheme: {
+            primary: '#F1041B',
+            secondary: '#fff',
+          },
+        });
+      });
+  };
 
-    return (
-        <Formik
-            initialValues={
-                {
-                    name: "",
-                    number: ""
-                }
-            }
-            validationSchema={contactSchema}
-
-            onSubmit={(values, actions) => {
-                console.log(values)
-                onAdd({ id: nanoid(5), ...values })
-                actions.resetForm();
-            }}>
-
-            <Form >
-
-                <div className={css.allForm}>
-                    <div className={css.form}>
-                        <label htmlFor={nameFieldId}>Name</label>
-                        <Field type="text" name="name" id={nameFieldId} />
-                        <ErrorMessage name="name" component="span" className={css.error} />
-
-                    </div>
-                    <div className={css.form}>
-                        <label htmlFor={numberFieldId}>Number</label>
-                        <Field type="text" name="number" id={numberFieldId} />
-                        <ErrorMessage className={css.error} name="number" component="span" />
-
-                    </div>
-                    <div className={css.buttonContainer}>
-                        <button type="submit" className={css.button}>Add contacts</button>
-                    </div>
-                </div>
-            </Form>
-        </Formik >
-
-    )
-}
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        number: '',
+      }}
+      validationSchema={contactSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
+        <div className={css.item}>
+          <label className={css.label} htmlFor={nameFieldId}>
+            Name
+          </label>
+          <Field className={css.input} type="text" name="name" id={nameFieldId} />
+          <ErrorMessage className={css.error} name="name" component="span" />
+        </div>
+        <div className={css.item}>
+          <label className={css.label} htmlFor={numberFieldId}>
+            Phone
+          </label>
+          <Field className={css.input} type="text" name="number" id={numberFieldId} />
+          <ErrorMessage className={css.error} name="number" component="span" />
+        </div>
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+};
