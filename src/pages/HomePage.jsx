7bdc141b870d48cf6c3a14aaf8e4d32755df3a3f
Julia24021991 <yuliya.kovalyk@gmail.com>
2024-03-13 20:27:49 +0200

    commit15

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import DocumentTitle from '../components/DocumentTitle/DocumentTitle';
+
+export default function HomePage() {
+  return (
+    <div>
+      <DocumentTitle>Home</DocumentTitle>
+    </div>
+  );
+}
